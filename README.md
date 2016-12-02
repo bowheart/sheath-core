b28ad6c9bbfd011e390a841297bbdc5b74eb8555
bowheart <kingdomdance602@gmail.com>
2016-12-01 23:48:06 -0700

    more documentation / add sheath.model() and sheath.object() methods

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sheath.js
 
-Sheath is a utility library for modular applications. It provides a clean, overly simplistic interface for defining modules. This includes dependency injection and (in browsers) asynchronous script loading &ndash; replacing RequireJS.
+Sheath is a utility library for modular applications. It provides a clean, overly simplistic interface for defining modules. This includes dependency injection and asynchronous script loading &ndash; replacing async libraries such as RequireJS.
 
 Sheath simplifies the application development process. It is specifically designed for use with MVC frameworks such as Backbone, React, and Mithril, but can also be used without a framework.
 
@@ -9,6 +9,49 @@ Sheath abstracts away the details of your module-web &ndash; you never have to w
 Basically, if you're wanting to create a modular application, Sheath is for you.
 
 Sheath is the module library.
+
+## The Basics
+
+### A simple example
+
+Let's create a module.
+
+```javascript
+sheath('dagger', function() {
+	return 'a dagger'
+})
+```
+
+Simple as that. Let's break down what happened here:
+
+- We *declared* a module named 'dagger'.
+- Sheath called our function. This *defined* our module.
+- We exposed the string 'a dagger' as our module's *visage*.
+
+Alright. Cool, I guess. Now what was the point of that?
+
+Let's create another module:
+
+```javascript
+sheath('assassin', 'dagger', function(dagger) {
+	return 'A deadly assassin. Weapon: ' + dagger
+})
+```
+
+Let's break this one down:
+
+- We declared a module named 'assassin' with one dependency, 'dagger'.
+- Sheath ensured that the 'dagger' module was loaded before:
+- Sheath called our function, passing the dagger module's visage as the first argument. This defined our module.
+- We exposed a new string concatenated with the dagger module's visage as our module's visage.
+
+Every module goes through this basic life cycle.
+
+> Tip: A module can be anything! Feel free to wrap every model, component, utility function, etc in its own module.
+
+### Implementing Async Loading
+
+
 
 ## Method API
 
@@ -23,7 +66,90 @@ Overloads:
 sheath(name : string, definition : function)
 ```
 
-This is the interfacing for defining modules.
+This is how you define a module.
+
+Arguments:
+
+**name** : string &ndash; *required* &ndash; This module's name. Used to identify this module for dependents and async loading. Can be any arbitrary string, e.g. `'my-module'` or `'namespace::submodule'`. You get to decide your naming conventions.
+
+**dependencies** : string|array &ndash; *optional* &ndash; The dependency or dependencies of this module given as a string or list of strings that match the names of other modules you've created or will create.
+
+**definition** : function &ndash; *required* &ndash; A function that encapsulates this module's definition. Once all dependencies of this module have been loaded, this function will be called and passed the loaded dependencies in order as they appeared in the dependencies list.
+
+Example:
+
+```javascript
+sheath('Weapon', function() {
+	return class Weapon {
+		get damage() {
+			return this._damage || 0
+		}
+	}
+})
+```
+
+```javascript
+sheath('Sword', 'Weapon', function(Weapon) {
+	return class Sword extends Weapon {
+		_damage: 10
+	}
+})
+```
+
+
+### sheath.asyncResolver()
+
+Signature:
+```javascript
+sheath.asyncResolver(newResolver : function)
+```
+
+The default async filename resolver
+This defines a mapping of module names to file paths.
+When the module loader is in async mode, it will call this function to get the value of each async script's `src` attribute.
+It will pass the module's name (e.g. `core`) and expect the file path to that module's location on the server (e.g. `modules/core/core.js`).
+
+#### How do I implement it?
+
+Define a function that takes a string and returns a string. Pass that function to `sheath.asyncResolver()`:
+
+```javascript
+// Take a module name (e.g. 'food.Pizza') and find the file path (e.g. 'modules/food/models/Pizza.js')
+sheath.asyncResolver(function(name) {
+	var nodes = name.split('.')
+	var file = nodes.pop() // get 'Pizza' from 'food.Pizza'
+	return 'modules/' + nodes.join('/') + '/' + (file[0].toLowerCase() !== file[0] ? 'models/' : '') + file + '.js'
+})
+```
+
+
+### sheath.waitingOn()
+
+A debugging utility.
+Call this from the console if you suspect the app is hanging.
+It will tell you the names of all the modules the app is waiting on.
+Look through them and see if any shouldn't be there.
+If they all look right, you probably have a circular dependency.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #### So what's so cool about it?
 
@@ -45,7 +171,7 @@ This helps you keep the Single Responsibility Principle, and who knows what othe
 
 - It's organized.
 A large application can turn into a big jumble of objects and helpers and config and your precious time really fast.
-Going modular (or 'sheathing') keeps everything in its own place, yet globally accessible (but not global)
+Going modular (or 'sheathing') keeps everything in its own place, yet globally accessible (but not global).
 
 
 #### How do I use it?
@@ -120,33 +246,3 @@ This can be hard to debug (though sheath.waitingOn() can help), but you should k
 2. A circular dependency. These are easy to avoid, but sometimes tricky to spot.
 These are usually solved by keeping strictly to the Single Responsibility Principle;
 separate the functionality that both modules need into a separate module that both require.
-
-
-### sheath.asyncResolver()
-
-This method is meant to be overridden.
-This defines a mapping of module names to file paths.
-When the module loader is in async mode, it will call this function to get the value of each async script's `src` attribute.
-It will pass the module's name (e.g. `core`) and expect the file path to that module's location on the server (e.g. `modules/core/core.js`).
-
-#### How do I implement it?
-
-Define a function that takes a string and returns a string. Pass that function to `sheath.asyncResolver()`:
-
-```javascript
-// Take a module name (e.g. 'food.Pizza') and find the file path (e.g. 'modules/food/models/Pizza.js')
-sheath.asyncResolver(function(name) {
-	var nodes = name.split('.')
-	var file = nodes.pop() // get 'Pizza' from 'food.Pizza'
-	return 'modules/' + nodes.join('/') + '/' + (file[0].toLowerCase() !== file[0] ? 'models/' : '') + file + '.js'
-})
-```
-
-
-### sheath.waitingOn()
-
-A debugging utility.
-Call this from the console if you suspect the app is hanging.
-It will tell you the names of all the modules the app is waiting on.
-Look through them and see if any shouldn't be there.
-If they all look right, you probably have a circular dependency.
--- src/sheath.js
+++ src/sheath.js
@@ -23,7 +23,9 @@
 		
 		// add() -- A new module was defined! If it's deps are already met, load it, otherwise add it to the list (this.toLoad)
 		add: function(module) {
+			if (module.name) this.declaredModules.push(module.name)
 			if (this.check(module)) return
+			
 			this.toLoad.push(module)
 			if (this.asyncMode) this.loadAsync(module.deps)
 		},
@@ -35,6 +37,7 @@
 				if (!this.loaded[dep]) return false // we found an unloaded dep; this module isn't ready
 			}
 			if (typeof index !== 'undefined') this.toLoad.splice(index, 1)
+			
 			this.load(module)
 			return true
 		},
@@ -53,7 +56,13 @@
 			var deps = module.deps.map(function(dep) {
 				return this.loaded[dep]
 			}.bind(this))
-			this.loaded[module.name] = module.defFunc.apply(module.defFunc, deps) || {}
+			
+			var definition = module.defFunc.apply(null, deps) || {} // TODO: add a way to specify the context of the defFunc
+			if (!module.name) return // none of the rest applies to nameless modules (modules added via sheath.run())
+			
+			if (this.loaded[module.name]) throw new Error('Sheath.js Error: Multiple modules with the same name found. Name: "' + module.name + '"')
+			
+			this.loaded[module.name] = definition
 			this.digest() // now that this module's loaded, digest to see if this module resolves any other module's last dep
 		},
 		
@@ -78,9 +87,7 @@
 		
 		// undeclaredModules() -- Returns the list of all modules that have been required but never declared.
 		undeclaredModules: function() {
-			var definedModules = Object.keys(this.loaded).concat(this.toLoad.map(function(module) {
-				return module.name
-			}))
+			var declaredModules = this.declaredModules
 			
 			return this.toLoad.map(function(module) {
 				return module.deps
@@ -88,15 +95,25 @@
 				for (var i = 0; i < nextDeps.length; i++) {
 					var nextDep = nextDeps[i]
 					
-					if (!~mem.indexOf(nextDep) && !~definedModules.indexOf(nextDep)) mem.push(nextDep)
+					if (!~mem.indexOf(nextDep) && !~declaredModules.indexOf(nextDep)) mem.push(nextDep)
 				}
 				return mem
 			}, [])
 		},
 		
 		asyncMode: false, // determines if we'll load any new deps asynchronously; will be turned on once all initial scripts are loaded
+		declaredModules: [], // keeps track of all module declarations we've encountered throughout the lifetime of this app.
 		loaded: {}, // the list of defined modules
 		toLoad: [] // the list of declared modules whose dependencies are not yet met
+	}
+	
+	// Turn all own properties of an object into their property descriptors
+	var toPropertyDescriptors = function(obj) {
+		var propertyDescriptors = {}
+		Object.keys(obj).forEach(function(key) {
+			propertyDescriptors[key] = Object.getOwnPropertyDescriptor(obj, key)
+		})
+		return propertyDescriptors
 	}
 	
 	
@@ -106,12 +123,12 @@
 		sheath() -- A utility for organized, encapsulated module definition and dependency injection.
 		
 		Parameters:
-			name -- required -- string -- The name of this module.
-			deps -- optional -- string | array -- The module or list of modules required by this module.
+			name : string -- required -- The name of this module.
+			deps : string | array -- optional -- The module or list of modules required by this module.
 				Each of these must be the name of another module defined somewhere.
-				If any dep in this list is not defined in the app, the app will 'hang' (see below).
+				If any dep in this list is not declared in the app and cannot be found by the async loader, the app will 'hang' (see below).
 				These will be 'injected' into the defFunc.
-			defFunc -- required -- function -- The function that will be called to define this module.
+			defFunc : function -- required -- The function that will be called to define this module.
 				The parameters of this function will be this module's dependencies, in the order they were listed.
 				Whatever is 'return'ed by this function will be injected into any modules that require this one.
 	*/
@@ -127,7 +144,7 @@
 	
 	
 	/*
-		sheath.async() / sheath.async(bool) -- a getter/setter to see/define status of the async loader.
+		sheath.async() / sheath.async(bool) -- a getter/setter to see/define the status of the async loader.
 		Pass true to activate async loading, false to deactivate.
 		Async loading is turned on by default.
 	*/
@@ -141,9 +158,88 @@
 		sheath.asyncResolver(function) -- the default async filename resolver doesn't do much. Use this guy to beef it up.
 	*/
 	sheath.asyncResolver = function(newResolver) {
-		if (typeof newResolver !== 'function') throw new TypeError('Sheath.js Error: custom asyncResolver must be a function')
+		if (typeof newResolver !== 'function') throw new TypeError('Sheath.js Error: Custom asyncResolver must be a function')
 		
 		asyncResolver = newResolver
+	}
+	
+	
+	/*
+		sheath.run() -- run some code that leverages Sheath's dependency injection, but without declaring a module.
+		The 'deps' arg is optional, so this can also be used for just arbitrary encapsulation -- replacing an IIFE, for example.
+	*/
+	sheath.run = function(deps, func) {
+		// arg swapping -- deps is optional; if 'func' doesn't exist, move it down.
+		if (!func) {
+			func = deps
+			deps = []
+		}
+		if (typeof deps === 'string') deps = [deps] // if 'deps' is a string, make it the only dependency
+		modules.add({deps: deps, defFunc: func})
+	}
+	
+	
+	/*
+		sheath.model() -- An easy interface for declaring JavaScript 'classes' with inheritance.
+		
+		Params:
+			parent : object|function -- optional -- The prototype, or function whose prototype this model will inherit.
+			model : object -- required -- The class definition. If the object has an 'init()' property (optional), it will become the class constructor.
+	*/
+	sheath.model = function(parent, model) {
+		// Arg swapping -- 'parent' is optional; if 'model' doesn't exist, move it down.
+		if (!model) {
+			model = parent
+			parent = custom.ModelBase // it's fine if custom.ModelBase is undefined
+		}
+		var parentPrototype = typeof parent === 'function' ? parent.prototype : parent
+		
+		// Make sure the model param is valid.
+		if (typeof model !== 'object') throw new TypeError('Sheath.js Error: Model must be an object')
+		
+		// Make sure the parent param is valid, if it was given.
+		if (parent && !~['function', 'object'].indexOf(typeof parent)) {
+			throw new TypeError('Sheath.js Error: Parent must be a constructor function or an object')
+		}
+		
+		// Use the init() property of the model as the constructor (if it exists), otherwise use the parent's (if there's a parent)
+		var constructor = model.init || parent && parentPrototype.init && function() {
+			return parentPrototype.init.apply(this, arguments)
+		} || new Function()
+		
+		// Implement the inheritance (if user gave a parent model) and make the rest of the model the prototype
+		constructor.prototype = Object.create(parentPrototype, toPropertyDescriptors(model))
+		
+		return constructor
+	}
+	
+	
+	/*
+		sheath.object() -- An easy interface for declaring JavaScript objects with custom and inheriting prototypes.
+		
+		Params:
+			parent : object -- optional -- The prototype object from which this object will inherit.
+			object : object -- required -- The new object you want created.
+	*/
+	sheath.object = function(parent, object) {
+		// Arg swapping -- 'parent' is optional; if 'object' doesn't exist, move it down.
+		if (!object) {
+			object = parent
+			parent = custom.ObjectBase // it's fine if custom.ObjectBase is undefined
+		}
+		
+		// Make sure the object param is valid.
+		if (typeof object !== 'object') throw new TypeError('sheath.object() error: object must be an object')
+		
+		// Make sure the parent param is valid, if it was given.
+		if (parent && typeof parent !== 'object') {
+			throw new TypeError('sheath.object() error: parent must be an object')
+		}
+		
+		// Make the parent the object's prototype.
+		if (parent) object = Object.create(parent, toPropertyDescriptors(object))
+		
+		return object
 	}
 	
 	
